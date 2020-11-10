da543a8907dd0afeba5a5a6cc6f18fe954fbf3aa
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-10 20:56:45 -0300

    criado menu de exibição

--- questao3/questao3.cpp
+++ questao3/questao3.cpp
@@ -140,6 +140,37 @@ void pesquisar()
   }
 }
 
+void exibir()
+{
+  int opcao_exibicao;
+
+  do
+  {
+    system("cls");
+    cout << "MENU DE EXIBICAO DOS FUNCIONARIOS \n" << endl;
+    cout << "[1] FUNCIONARIOS COM SALARIO INFERIOR A R$ 500" << endl;
+    cout << "[2] FUNCIONARIOS COM SALARIO INFERIOR A R$ 1000" << endl;
+    cout << "[3] FUNCIONARIOS COM SALARIO SUPERIOR A R$ 4000" << endl;
+    cout << "[4] FUNCIONARIOS COM SALARIO SUPERIOR OU IGUAL A R$ 5000" << endl;
+    cout << "[5] FUNCIONARIOS COM SALARIO SUPERIOR OU IGUAL A R$ 7000" << endl;
+    cout << "[6] FUNCIONARIOS COM SALARIO SUPERIOR OU IGUAL A R$ 10000" << endl;
+    cout << "[7] TODOS OS FUNCIONARIOS" << endl;
+    cout << "[0] VOLTAR" << endl << endl;
+    cout << "OPCAO: ";
+    cin >> opcao_exibicao;
+
+    switch (opcao_exibicao)
+    {
+    case 1:
+      /* code */
+      break;
+    
+    default:
+      break;
+    }
+  } while (opcao_exibicao != 0);
+}
+
 main()
 {
   int opcao_usuario;
@@ -165,6 +196,10 @@ main()
 
     case 2:
       pesquisar();
+      break;
+    
+    case 3:
+      exibir();
       break;
     
     default:
