39db2c8f14740b835136b94c87cabf41d7194631
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-11 16:01:59 -0300

    exibição funcionando

--- questao3/questao3.cpp
+++ questao3/questao3.cpp
@@ -140,9 +140,46 @@ void pesquisar()
   }
 }
 
+void exibiSalarioFuncionario(int opcao_desejada, float valor_estabelecido)
+{
+  if (opcao_desejada == 1 || opcao_desejada == 2)
+  {
+    for (int cadastrado = 0; cadastrado < quantidade_funcionarios_registrados; cadastrado++)
+    {
+      if (servidor[cadastrado].salario_liquido < valor_estabelecido)
+        cout << servidor[cadastrado].nome << " R$ " << servidor[cadastrado].salario_liquido << endl;
+    }
+  }
+  else if (opcao_desejada == 3)
+  {
+    for (int cadastrado = 0; cadastrado < quantidade_funcionarios_registrados; cadastrado++)
+    {
+      if (servidor[cadastrado].salario_liquido > valor_estabelecido)
+        cout << servidor[cadastrado].nome << " R$ " << servidor[cadastrado].salario_liquido << endl;
+    }
+  }
+  else
+  {
+    for (int cadastrado = 0; cadastrado < quantidade_funcionarios_registrados; cadastrado++)
+    {
+      if (servidor[cadastrado].salario_liquido >= valor_estabelecido)
+        cout << servidor[cadastrado].nome << " R$ " << servidor[cadastrado].salario_liquido << endl;
+    }
+  }
+
+  system("pause");
+}
+
+void exibiTodosSalariosFuncionario()
+{
+  for (int cadastrado = 0; cadastrado < quantidade_funcionarios_registrados; cadastrado++)
+    cout << servidor[cadastrado].nome << " R$ " << servidor[cadastrado].salario_liquido << endl;
+}
+
 void exibir()
 {
   int opcao_exibicao;
+  const float menor_de_500 = 500, menor_de_1K = 1000, maior_de_4K = 4000, maior_ou_igual_5K = 5000, maior_ou_igual_7K = 7000, maior_ou_igual_10K = 10000;
 
   do
   {
@@ -159,14 +196,59 @@ void exibir()
     cout << "OPCAO: ";
     cin >> opcao_exibicao;
 
+    system("cls");
+    if (quantidade_funcionarios_registrados == 0)
+    {
+      cout << "\t\t*** MENSAGEM DE ERRO ***\n\tNAO HA NENHUM FUNCIONARIO REGISTRADO NO SISTEMA\n\nPOR FAVOR ADICIONE UM NOVO FUNCIONARIO OU CARREGUE OS DADOS DE UM ARQUIVO EXTERNO\n\n";
+      system("pause");
+      return;
+    }
+
     switch (opcao_exibicao)
     {
-    case 1:
-      /* code */
-      break;
-    
-    default:
-      break;
+      case 1:
+        cout << "FUNCIONARIOS COM SALARIO INFERIOR A R$ 500\n\n";
+        exibiSalarioFuncionario(opcao_exibicao, menor_de_500);
+        break;
+      
+      case 2:
+        cout << "FUNCIONARIOS COM SALARIO INFERIOR A R$ 1000\n\n";
+        exibiSalarioFuncionario(opcao_exibicao, menor_de_1K);
+        break;
+        
+      case 3:
+        cout << "FUNCIONARIOS COM SALARIO SUPERIOR A R$ 4000\n\n";
+        exibiSalarioFuncionario(opcao_exibicao, maior_de_4K);
+        break;
+        
+      case 4:
+        cout << "FUNCIONARIOS COM SALARIO SUPERIOR OU IGUAL A R$ 5000\n\n";
+        exibiSalarioFuncionario(opcao_exibicao, maior_ou_igual_5K);
+        break;
+        
+      case 5:
+        cout << "FUNCIONARIOS COM SALARIO SUPERIOR OU IGUAL A R$ 7000\n\n";
+        exibiSalarioFuncionario(opcao_exibicao, maior_ou_igual_7K);
+        break;
+        
+      case 6:
+        cout << "FUNCIONARIOS COM SALARIO SUPERIOR OU IGUAL A R$ 10000\n\n";
+        exibiSalarioFuncionario(opcao_exibicao, maior_ou_igual_10K);
+        break;
+
+      case 7:
+        cout << "TODOS OS FUNCIONARIOS\n\n";
+        exibiTodosSalariosFuncionario();
+        system("pause");
+        break;
+
+      case 0:
+        break; 
+      
+      default:
+        cout << "\n\t*** OCORREU UM ERRO NA ENTRADA DA OPCAO ***" << endl << endl;
+        system("pause");
+        break;
     }
   } while (opcao_exibicao != 0);
 }
